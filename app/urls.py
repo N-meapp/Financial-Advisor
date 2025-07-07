94820bc35972929e8372b02afec24cefbb601a2d
N-meapp <developersnme@gmail.com>
2025-07-07 15:51:40 -0700

    second commit

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),  # adjust 'index' to your view name
     path('register/', views.register, name='register'), 
+    path('blog/', views.blog, name='blog'), 
+    path('investor/', views.investor, name='investor'), 
+    path('contact/', views.contact, name='contact'), 
+
+
+
 ]
--- app/views.py
+++ app/views.py
@@ -6,7 +6,12 @@ def index(request):
 
 def register(request):
     return render(request,'registration.html')
+
+def blog(request):
+    return render(request,'blog.html')
  
+def investor(request):
+    return render(request,'investing insights.html')
 
-
-
+def contact(request):
+    return render(request,'contact.html')
